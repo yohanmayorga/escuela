b32e00ad4f89b55fe2a111ee9e8ef28d543ee662
Yohan Mayorga <yohanucv@gmail.com>
2024-04-27 18:38:24 -0400

    mobile menu test

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -5,6 +5,13 @@ import logo from "@/../../public/assets/logo.png";
 import Image from "next/image";
 import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import { MdOutlineEmail } from "react-icons/md";
+import Link from "next/link";
+import { nosotros } from "@/utils/utilsNavbar";
+import { oferta } from "@/utils/utilsNavbar";
+import { admision } from "@/utils/utilsNavbar";
+import { comunidad } from "@/utils/utilsNavbar";
 
 const MobileNavbar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -33,7 +40,38 @@ const MobileNavbar = () => {
       </div>
 
       <div className={styles.menuBody}>
-        {showMenu && <div className={styles.container}>Hola</div>}
+        {showMenu && (
+          <div className={styles.container}>
+            <div className={styles.links}>
+              <span className={styles.link}>
+                <span className={styles.linkItem}>
+                  Nosotros
+                  <IoMdArrowDropdownCircle />
+                </span>
+                <div className={styles.subMenu}>
+                  {nosotros.map((item: any) => {
+                    return (
+                      <Link
+                        key={item.name}
+                        href={item.link}
+                        className={styles.link2}
+                      >
+                        {item.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </span>
+
+              <Link href="#" className={styles.link}>
+                <span className={styles.button}>
+                  <MdOutlineEmail />
+                  Contacto
+                </span>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
