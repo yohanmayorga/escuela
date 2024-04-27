866ca52b2b6db1d4c60cabc030922612d77205f3
Yohan Mayorga <yohanucv@gmail.com>
2024-04-27 17:16:03 -0400

    mobile menu fixed

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "@/styles/MobileNavbar.module.css";
 import logo from "@/../../public/assets/logo.png";
 import Image from "next/image";
 import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const MobileNavbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    return setShowMenu(!showMenu);
+  };
+
   return (
     <div className={styles.body}>
       <div className={styles.header}>
@@ -15,8 +23,12 @@ const MobileNavbar = () => {
             <span>Carmen Maizo de Bello</span>
           </div>
         </div>
-        <div className={styles.right}>
-          <IoMenu className={styles.menu} />
+        <div className={styles.right} onClick={() => handleClick()}>
+          {showMenu ? (
+            <IoClose className={styles.menu} />
+          ) : (
+            <IoMenu className={styles.menu} />
+          )}
         </div>
       </div>
     </div>
