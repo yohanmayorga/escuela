31fa6a28f4af5810d443c7b6e20acc307f8e111d
Yohan Mayorga <yohanucv@gmail.com>
2024-04-27 22:11:20 -0400

    menu

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -9,6 +9,8 @@ import { IoMdArrowDropdownCircle } from "react-icons/io";
 import { IoMdArrowDropupCircle } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa";
+import { IoMdTime } from "react-icons/io";
+import { FaMapMarkedAlt } from "react-icons/fa";
 import Link from "next/link";
 import { nosotros } from "@/utils/utilsNavbar";
 import { oferta } from "@/utils/utilsNavbar";
@@ -16,7 +18,7 @@ import { admision } from "@/utils/utilsNavbar";
 import { comunidad } from "@/utils/utilsNavbar";
 
 const MobileNavbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   const [showSubmenu, setShowSubmenu] = useState(false);
   const [id, setId] = useState("");
 
@@ -84,6 +86,19 @@ const MobileNavbar = () => {
       <div className={styles.menuBody}>
         {showMenu && (
           <div ref={ref} className={styles.container}>
+            <div className={styles.profileContainer}>
+              <div className={styles.profile}>
+                <Image
+                  src={logo}
+                  alt="logo"
+                  className={styles.logoProfile}
+                  priority
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <span>E.B.N.</span>
+              <span>Carmen Maizo de Bello</span>
+            </div>
             <div className={styles.links}>
               <span className={styles.link}>
                 <span
@@ -216,11 +231,20 @@ const MobileNavbar = () => {
                   })}
                 </div>
               </span>
+            </div>
 
-              <div className={styles.bottom}>
-                <div className={styles.bottomItem}>
-                  <FaPhone /> (0212)-1234567
-                </div>
+            <div className={styles.bottom}>
+              <div className={styles.bottomItem}>
+                <FaPhone /> (0212)-1234567
+              </div>
+              <div className={styles.bottomItem}>
+                <MdOutlineEmail /> correo@gmail.com
+              </div>
+              <div className={styles.bottomItem}>
+                <IoMdTime /> 08:00 am - 04:00 pm
+              </div>
+              <div className={styles.bottomItem}>
+                <FaMapMarkedAlt /> El Valle - Caracas
               </div>
             </div>
           </div>
